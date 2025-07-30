a3f7eab91f3db36a6fb0907a30f4301605b4d2f8
Kshitij Halmare <horizon29042004@gmail.com>
2025-07-30 14:24:36 +0530

    User registerion and user fetrching  done

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -1,6 +1,57 @@
 import express from "express"
+import User from "../Schemas/UserSchema"
 const router=express.Router()
 
-router.post('/register',(req,res)=>{
-    
+// router.post('/register',(req,res)=>{
+//     const {name,email,phoneno,age}=req.body;
+//     if(!name || !email || !phoneno || !age){
+//         return res.status(400).json({message:"All feilds are required"});
+//     }
+//     const user=new User({name,email,phoneno,age});
+//     user.save();
+//     res.status(200).json({message:"Iser registered successfully",user});
+// })
+
+router.post("/register",async (req,res)=>{
+    const {name,email,phoneno,age}=req.body;
+    if(!namee || !email || !phoneno || !age){
+        return res.status(200).json({
+            message:"All feilds are required"
+        })
+    }
+    const exisitingUser=await User.find({email:email});
+    if(exisitingUser){
+        return res.status(400).json({
+            message:"User Already exists with this email id",
+            user:exisitingUser
+        })
+    }else{
+        const user=new User({email,phoneno,name,age});
+        await user.save();
+        return res.status(200).json({
+            message:"User registered successfully",
+            user:user
+        })
+    }
+})
+router.get('/AllUser',async (req,res)=>{
+    const user=await User.find();
+    return res.statuus(200).json({
+        message:"All user retrieved successfully",
+        Users:user
+    })
+})
+router.get('/user/:id',async(req,res)=>{
+    const userId=req.params.id;
+    const user=await User.find({email:userId});
+    if(user){
+        return res.status(200).json({
+            message:"User found Successfully",
+            user:user
+        })
+    }else{
+        return res.statuse(404).json({
+            message:"User not found"
+        })
+    }
 })
--- Schemas/UserSchema.js
+++ Schemas/UserSchema.js
@@ -1,1 +1,26 @@
 import mongoose from "mongoose";
+const userSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true
+    },
+    phoneno:{
+        type:Number,
+        required:true,
+        unique:true
+    },
+    age:{
+        type:Number,
+        required:true,
+    }
+});
+const User=mongoose.nodle('User',mongooseSchema);;
+export default User;
